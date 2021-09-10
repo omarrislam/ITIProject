4eb0fc95c0b2b82cf503db46b07965913d3a64d5
omarrislam <omareslam07@gmail.com>
2021-09-11 00:53:05 +0200

    l

--- src/Component/shared/header.js
+++ src/Component/shared/header.js
@@ -6,14 +6,14 @@ export default class Header extends React.Component {
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <form class="d-flex ms-auto">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <form class="d-flex ms-auto">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="#">Home</a>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'
 import './Component/shared/header.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
